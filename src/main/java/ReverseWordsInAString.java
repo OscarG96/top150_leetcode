6acf8d391a7146653bbc9fd7004486bc5ffd6315
Oscar <oscar.gtzv@hotmail.com>
2025-08-12 22:08:21 -0600

    challenge done

---
+++ src/main/java/ReverseWordsInAString.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ReverseWordsInAString {
+    public static String reverseWords(String s) {
+        String[] splitString = s.trim().split("\\s+");
+        ArrayList<String> result = new ArrayList<>();
+        for (int i = splitString.length - 1; i >= 0; i--) {
+            result.add(splitString[i]);
+        }
+        return String.join(" ", result);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverseWords("a good   example"));
+    }
+}
