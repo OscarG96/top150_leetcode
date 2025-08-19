c8f91c3913e0c2dfa6cb163f491a5d2c95d39b6b
Oscar <oscar.gtzv@hotmail.com>
2025-08-18 22:58:04 -0600

    challenge done

---
+++ src/main/java/FindTheIndexOfTheFirstOccurrenceInAString.java
@@ -0,0 +1,10 @@
+public class FindTheIndexOfTheFirstOccurrenceInAString {
+    public static int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(strStr("sadbutsad", "sad"));
+        System.out.println(strStr("leetcode", "leeto"));
+    }
+}
