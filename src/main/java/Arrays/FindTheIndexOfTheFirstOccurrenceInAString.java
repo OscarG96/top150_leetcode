ba620ca7e8d360c6f358885db4234e14fea6d19a
Oscar <oscar.gtzv@hotmail.com>
2025-08-27 18:26:04 -0600

    change logic to brute force

--- src/main/java/Arrays/FindTheIndexOfTheFirstOccurrenceInAString.java
+++ src/main/java/Arrays/FindTheIndexOfTheFirstOccurrenceInAString.java
@@ -2,7 +2,19 @@ package Arrays;
 
 public class FindTheIndexOfTheFirstOccurrenceInAString {
     public static int strStr(String haystack, String needle) {
-        return haystack.indexOf(needle);
+
+        int n = haystack.length();
+        int m = needle.length();
+
+        if (m == 0) return 0; // edge case
+
+        for (int i = 0; i <= n - m; i++) {
+            if (haystack.substring(i, i + m).equals(needle)) {
+                return i;
+            }
+        }
+
+        return -1;
     }
 
     public static void main(String[] args) {
