e74a95868a801b638946e7838289e612965e0f37
Oscar <oscar.gtzv@hotmail.com>
2025-08-27 18:25:53 -0600

    challenge done

--- src/main/java/TwoPointers/ValidPalindrome.java
+++ src/main/java/TwoPointers/ValidPalindrome.java
@@ -1,4 +1,25 @@
 package TwoPointers;
 
 public class ValidPalindrome {
+    public static boolean isPalindrome(String s) {
+
+        String t = s.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
+        boolean result = true;
+        int left = 0;
+        int right = t.length() - 1;
+        while (left < right) {
+            if (t.charAt(left) != t.charAt(right)) {
+                result = false;
+                break;
+            }
+            left++;
+            right--;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String s = " ";
+        System.out.println(isPalindrome(s));
+    }
 }
