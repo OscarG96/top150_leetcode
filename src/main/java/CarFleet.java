b583e73bce8c2abb91a3f63b2c71a454da77faa7
Oscar <oscar.gtzv@hotmail.com>
2025-09-22 17:33:29 -0600

    challenge done

---
+++ src/main/java/CarFleet.java
@@ -0,0 +1,14 @@
+public class CarFleet {
+    public static int carFleet(int target, int[] position, int[] speed) {
+        for (int i = 0; i < target; i++) {
+            
+        }
+        return 1;
+    }
+
+    public static void main(String[] args) {
+        int target = 12;
+        int[] postion = {10,8,0,5,3};
+        int[] speed = {2,4,1,1,3};
+    }
+}
---
+++ src/main/java/TwoPointers/TwoSum2.java
@@ -0,0 +1,22 @@
+package TwoPointers;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class TwoSum2 {
+    public static int[] twoSum(int[] numbers, int target) {
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < numbers.length; i++) {
+            int diff = target - numbers[i];
+            if (map.containsKey(diff)) {
+                return new int[]{map.get(diff) + 1, i + 1};
+            }
+            map.put(numbers[i], i);
+        }
+        return new int[]{0,0};
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(twoSum(new int[]{-1,0}, -1)));
+    }
+}
